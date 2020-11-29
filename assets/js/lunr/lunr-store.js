106aeaba9be24469b41764e78f51270290179f3a
Chao Lin <clinmazda@gmail.com>
2020-11-29 14:02:24 -0500

    Uploaded the whole site.

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Create a table of content with links for PDF",
+        "excerpt":"Did you ever think to create a table of content with links to a PDF ebook without a proprietary software? Here is a solution by adding a table of conetent in the form of hyperlink hierarchical bookmarks with PDFtk. Installation of PDFtk Windows Downlad HERE, and execute a normal installation....","categories": ["PDF"],
+        "tags": ["PDFtk","bookmark"],
+        "url": "https://cleanpages.github.io/website/pdf/add-pdf-table-of-content/",
+        "teaser": null
+      }]
