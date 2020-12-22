76131eaed87474698e22b0deb583ea06e0728574
Chao Lin <clinmazda@gmail.com>
2020-12-22 14:10:47 -0500

    Updated all posts URL

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Create a table of content with links for PDF",
         "excerpt":"Did you ever think to create a table of content with links to a PDF ebook without a proprietary software? Here is a solution by adding a table of conetent in the form of hyperlink hierarchical bookmarks with PDFtk. Installation of PDFtk Windows Downlad HERE, and execute a normal installation....","categories": ["PDF"],
         "tags": ["PDFtk","bookmark"],
-        "url": "https://cleanpages.github.io/website/pdf/add-pdf-table-of-content/",
+        "url": "https://cleanpages.github.io/pdf/add-pdf-table-of-content/",
         "teaser": null
       },{
         "title": "JDK/JRE Installation on Linux",
         "excerpt":"JDK installation and management are easy and clear in Linux with help of the software tools integrated in Linux, which are tar and update-alternatives. This article consists of download, installation, and management of multiple versions of JDK. Download JDK: The easiest way to install JDK in Debian based linux distro...","categories": ["JAVA"],
         "tags": ["JDK","JRE","JRE"],
-        "url": "https://cleanpages.github.io/website/java/install-jdk/",
+        "url": "https://cleanpages.github.io/java/install-jdk/",
         "teaser": null
       },{
         "title": "More usage of pdftk",
         "excerpt":"Here is more usage of pdftk. This post keeps being updated… Note: If you want to know how to install pdftk in Linux, please refer to Create a table of content with links for PDF Merge multiple PDF files to one PDF files Change into the directory where the PDF...","categories": ["PDF"],
         "tags": ["PDFtk"],
-        "url": "https://cleanpages.github.io/website/pdf/pdftk-more-usage/",
+        "url": "https://cleanpages.github.io/pdf/pdftk-more-usage/",
         "teaser": null
       },{
         "title": "Make a new hard drive using ext4 file system",
         "excerpt":"This article is applicable to external (mainly USB) storage device as well as internal storage device. Please note that all disk operation in Linux requires super user privileges. Find the new hard drive The command lsblk will list all detected hard drives. NAME MAJ:MIN RM SIZE RO TYPE MOUNTPOINT sda...","categories": ["Linux"],
         "tags": ["Partition","File System","Format","Disk"],
-        "url": "https://cleanpages.github.io/website/linux/external-hdd-ext4/",
+        "url": "https://cleanpages.github.io/linux/external-hdd-ext4/",
         "teaser": null
       }]
